8859395309d1fa8dd5341d0689a3cc895b178c7f
Felipe Moura <felipefurtado.2@gmail.com>
2021-11-10 22:49:28 -0300

    Multiplicação de Valores

--- Calculadora.js
+++ Calculadora.js
@@ -1,15 +1,15 @@
-function sum(){
-    //TODO: Implementar soma
+function sum() {
+  //TODO: Implementar soma
 }
 
-function sub(){
-    //TODO: Implementar subtração
+function sub() {
+  //TODO: Implementar subtração
 }
 
-function mult(){
-    //TODO: Implementar multiplicação
+function mult(a,b) {
+   return a * b;
 }
 
-function div(){
-    //TODO: Implementar divisão
+function div() {
+  //TODO: Implementar divisão
 }
